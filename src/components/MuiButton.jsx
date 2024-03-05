959e8a77836a14ac9102e1ce0245111ea17575d6
Fahim <fahimarefin57@gmail.com>
2024-03-05 21:08:21 +0600

    Button done (video lecture upto 6 is done)

--- src/components/MuiButton.jsx
+++ src/components/MuiButton.jsx
@@ -1,6 +1,23 @@
-import { Button, Stack } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  IconButton,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
 
 function MuiButton() {
+  const [alignment, setAlignment] = useState([]);
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  console.log("alignment", alignment);
+
   return (
     <Stack spacing={4}>
       {/* normal buttons start */}
@@ -95,15 +112,78 @@ function MuiButton() {
       </Stack>
       {/* contained color buttons with different size ends */}
 
-      {/* button with icons */}
-      <Stack direction="row" spacing={2}>
-        <Button variant="contained" color="primary">
-          asd
+      {/* button with icons, iconButton, disabble ripple,elevation starts*/}
+      <Stack display="block" direction="row" spacing={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("clicked!")}
+        >
+          send
         </Button>
-        <Button variant="contained" color="secondary">
-          asd
+        <Button
+          variant="contained"
+          color="primary"
+          endIcon={<SendIcon />}
+          disableElevation
+        >
+          send
         </Button>
+        <IconButton aria-label="icon" color="info" size="large">
+          <SendIcon />
+        </IconButton>
       </Stack>
+      {/* button with icons, iconButton, disabble ripple,elevation starts*/}
+
+      {/* Button groups start*/}
+      <Stack direction="row">
+        <ButtonGroup variant="contained" orientation="vertical">
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack direction="row">
+        <ButtonGroup variant="text" orientation="horizontal">
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack direction="row">
+        <ButtonGroup
+          variant="outlined"
+          orientation="horizontal"
+          color="success"
+          size="small"
+          aria-label="small outlined button group"
+        >
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      {/* Button groups end*/}
+
+      {/* Button groups starts */}
+      <Stack direction="row">
+        <ToggleButtonGroup
+          color="primary"
+          value={alignment}
+          onChange={handleChange}
+          aria-label="Platform"
+          // exclusive
+          size="small"
+          orientation="vertical"
+        >
+          <ToggleButton value="web">Web</ToggleButton>
+          <ToggleButton value="android">Android</ToggleButton>
+          <ToggleButton value="ios">iOS</ToggleButton>
+        </ToggleButtonGroup>
+      </Stack>
+      {/* Button groups starts */}
     </Stack>
   );
 }
